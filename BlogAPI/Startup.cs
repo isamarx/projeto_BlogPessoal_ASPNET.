67c9cd7a0fb747c4ed5105a70893f24d8cefca76
Samara Pereira <samarapds26@gmail.com>
2022-08-03 10:49:38 -0300

    @isamarx [Task: Criação de modelos e contexto da aplicação] {02/08/2022 17:15}

--- BlogAPI/Startup.cs
+++ BlogAPI/Startup.cs
@@ -1,6 +1,8 @@
+using BlogAPI.Src.Contextos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,6 +16,8 @@ namespace BlogAPI
 {
     public class Startup
     {
+        private object contexto;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,17 +28,32 @@ namespace BlogAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Adicionando contexto 
+            services.AddDbContext<BlogPessoalContexto>(opt =>
+            opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
 
+            //Adicionando serviço de Controladores
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BlogPessoalContexto contexto)
         {
             if (env.IsDevelopment())
             {
+                contexto.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
+
             }
+
+            // Ambiente de produção
+            contexto.Database.EnsureCreated();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
 
             app.UseRouting();
 
